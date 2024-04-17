42e83dda9b28ab04ef2f35d6e6fbf13be21c7250
ghorg2208 <victor.millet92@gmail.com>
2024-04-17 12:06:08 +0200

    cc

--- middlewares/logger.js
+++ middlewares/logger.js
@@ -1,8 +1,21 @@
-// middlewares/logger.js
-
-const loggerMiddleware = (req, res, next) => {
-    console.log('nouvelle requête entrante');
+const logHeadersMiddleware = (req, res, next) => {
+    console.log('Headers de la requête :', req.headers);
     next();
   };
+
+  const firewallMiddleware = (req, res, next) => {
+    const unrestrictedUrls = ['/hello', '/public'];
+    const requestedUrl = req.url;
+    const token = req.headers.authorization;
   
-  module.exports = loggerMiddleware;
+    if (unrestrictedUrls.includes(requestedUrl)) {
+      // URL non restreinte, passer au prochain middleware
+      next();
+    } else if (token === 'valid_token') {
+      // Token valide, passer au prochain middleware
+      next();
+    } else {
+      // Accès refusé
+      res.status(403).json({ message: 'Accès refusé' });
+    }
+  };
